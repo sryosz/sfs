7f6ab4be663167ee2e4d4701c5a004d758267ef2
sryosz <workbin.osz@gmail.com>
2025-01-31 16:01:00 +0200

    Add encoding

---
+++ internal/encoding/encoding.go
@@ -0,0 +1,26 @@
+package sfsencoding
+
+import (
+	"io"
+
+	"github.com/sryosz/sharing-file-system/internal/message"
+)
+
+type Decoder interface{
+	Decode(r io.Reader, msg *message.PeerMsg) error
+}
+
+type DefaultDecoder struct{}
+
+func (d *DefaultDecoder) Decode(r io.Reader, msg *message.PeerMsg) error{
+	buf := make([]byte, 1028)
+	n, err := r.Read(buf)
+	if err != nil {
+		return err
+	}
+
+	msg.Payload = buf[:n]
+
+	return nil
+}
+
